3fa3966ede3346c1faca556646a7a0d8f7e8037a
Aleksandr <sanhes95@gmail.com>
2022-08-18 14:08:29 +0300

    WebSocket

--- src/main/java/com/github/san4ezuz/flightsposition/FlightRetriever.java
+++ src/main/java/com/github/san4ezuz/flightsposition/FlightRetriever.java
@@ -3,7 +3,10 @@ package com.github.san4ezuz.flightsposition;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -11,6 +14,7 @@ import java.util.function.Consumer;
 @Configuration
 public class FlightRetriever {
     private final FlightRepository flightRepository;
+    private final WebSocketHandler webSocketHandler;
 
     @Bean
     Consumer<List<Flight>> retrieveFlight() {
@@ -18,7 +22,20 @@ public class FlightRetriever {
         return flList -> {
             flightRepository.deleteAll();
             flightRepository.saveAll(flList);
-            flightRepository.findAll().forEach(System.out::println);
+            sendPositions();
         };
+    }
+
+    private void sendPositions() {
+        if (flightRepository.count() > 0) {
+            for (WebSocketSession sessionInList : webSocketHandler.getSessionList()) {
+                try {
+                    sessionInList.sendMessage(new TextMessage(flightRepository.findAll().toString()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+
+                }
+            }
+        }
     }
 }
